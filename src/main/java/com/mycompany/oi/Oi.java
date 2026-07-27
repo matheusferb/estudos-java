f292ad5d70611b322adc7241be9639fc9e67e3a0
Matheus Fernandes <matheusferborges123@gmail.com>
2026-07-27 19:58:45 -0300

    "Primeiro commit"

---
+++ src/main/java/com/mycompany/oi/Oi.java
@@ -0,0 +1,15 @@
+package com.mycompany.oi;
+
+public class Oi {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("oi mundinho");
+        int a = 10;
+        int b = 2;
+        System.out.println(a);
+        double y = a*b;
+        System.out.println(y);
+    }
+}
+
